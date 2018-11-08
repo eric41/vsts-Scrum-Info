d581d8d20663c316078e88822f30f838c1061cc3
RBAMOUSER\subiraa2 <toni.subirana@roche.com>
2018-11-08 16:19:48 +0100

    Showing results in a TFS dialog

--- scripts/app.ts
+++ scripts/app.ts
@@ -2,7 +2,7 @@
 
 import { MessageHelper } from "./logic/messageHelper";
 import { WorkItemCalculations } from "./logic/workItemCalculations";
-
+import Dialogs = require("VSS/Controls/Dialogs");
 
 var actionProvider = {
     getMenuItems: (context) => {
@@ -24,20 +24,37 @@ var actionProvider = {
                     witClient.getWorkItems(workitemidlist, null, null, 0).then(
                         function (workItems) {
                             let calculations = new WorkItemCalculations(workItems);
-                            //alert(calculations.getWorkItemsResults());
+                            alert(calculations.getWorkItemsResults());
+                            
+                            $("#show").click(() => {
+                                Dialogs.show(Dialogs.ModalDialog, {
+                                    title: "ScrumInfo",
+                                    contentText: calculations.getWorkItemsResults(),
+                                    buttons: null
+                                });
+                            });
+
+                            /*debugger;
+
 
                             var node = document.createElement("div");
                             node.className = "velocity-chart small-chart-container";
                             node.innerText = calculations.getWorkItemsResults();
 
                             var node2 = document.createElement("div");
+                            node2.style.position = "absolute";
+                            node2.style.top = "10px";
+                            node2.style.width = "100%";
+                            node2.style.textAlign = "center";
+                            node2.style.background = "white";
+                            node2.style.fontSize = "small";
                             node2.appendChild(node);
 
-                            var destination = document.querySelectorAll("div.backlog-hub-tile-region")[0];
-                            destination.insertBefore(node2, destination.firstChild);
+                            var destination = document.querySelectorAll("body")[0];
+                            destination.insertBefore(node2, destination.firstChild);*/
                         });
                 });
-                //debugger;
+                debugger;
             }
         }];
     }
